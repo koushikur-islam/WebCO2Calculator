1073cd0bfd004e5a7582d597a117fcdc9e47939c
koushikur-islam <koushikur.aiub@gmail.com>
2023-10-20 18:08:09 +1100

    update

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -3,8 +3,8 @@ export default function FooterComponent() {
         <div className="text-center py-5" style={{ backgroundColor: '#222831' }}>
             <div className="limited-box md:mb-0" style={{ color: 'var(--muted-bg)' }}>
                 <div>
-                    <p className='m-0 p-0 inline-block text-2xl font-bold text-green-600'>Eco</p>
-                    <p className='m-0 p-0 inline-block text-2xl font-bold'>Browse</p>
+                    <p className='m-0 p-0 inline-block text-2xl font-bold text-green-600 overflow-y-hidden'>Eco</p>
+                    <p className='m-0 p-0 inline-block text-2xl font-bold overflow-y-hidden'>Browse</p>
                 </div>
                 <p className="py-2">
                 Verify your website's carbon emission. Raise awareness for a better tomorrow.
